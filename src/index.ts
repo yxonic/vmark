0408a3e649b5e692a60842238834c20594cc4847
Yu Yin <yxonic@gmail.com>
2021-11-30 12:04:01 +0800

    feat: basic rendering

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export default class VMark {}
+import { defineComponent } from 'vue'
+import { VueRenderer } from './renderer'
+
+export default defineComponent({
+  name: 'VMark',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const renderer = new VueRenderer()
+    return () => {
+      renderer.render(props.src)
+    }
+  },
+})
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,1 +1,14 @@
-export class VueRenderer {}
+import MarkdownIt from 'markdown-it'
+export class VueRenderer {
+  md: MarkdownIt
+  constructor() {
+    this.md = new MarkdownIt({
+      html: true,
+      linkify: true,
+      typographer: true,
+    })
+  }
+  render(src: string) {
+    return this.md.render(src)
+  }
+}
