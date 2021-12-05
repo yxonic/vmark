9ba430402912641b562749918ef8f5ed5f0eb8dd
Yu Yin <yxonic@gmail.com>
2021-12-06 00:45:49 +0800

    fix: test

--- test/vue.test.ts
+++ test/vue.test.ts
@@ -1,6 +1,6 @@
 import VMark from '../src'
 import { createSSRApp, h } from 'vue'
-import { renderToString } from '@vue/server-renderer'
+import { renderToString } from 'vue/server-renderer'
 
 it('should render markdown as vue component', async () => {
   const app = createSSRApp({
